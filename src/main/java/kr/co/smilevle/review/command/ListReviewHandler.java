6cc50b5150337d79f36150957e6c5af145ce618e
JaeKwang Park <qo010478@gmail.com>
2021-08-23 15:47:18 +0900

    리뷰 조회 기능 구현(handler 클래스 작성)

---
+++ src/main/java/kr/co/smilevle/review/command/ListReviewHandler.java
@@ -0,0 +1,27 @@
+package kr.co.smilevle.review.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.smilevle.common.command.CommandHandler;
+import kr.co.smilevle.review.service.ListReviewService;
+import kr.co.smilevle.review.service.ReviewPage;
+
+public class ListReviewHandler implements CommandHandler{
+	
+	private ListReviewService listService = new ListReviewService();
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String pageNoVal = request.getParameter("PageNo");
+		int pageNo = 1;
+		if(pageNoVal != null) {
+			pageNo = Integer.parseInt(pageNoVal);
+		}
+		ReviewPage reviewPage = listService.getReviewPage(pageNo);
+		request.setAttribute("reviewPage", reviewPage);
+		return "/WEB-INF/review.jsp";
+	}
+	
+	
+}
