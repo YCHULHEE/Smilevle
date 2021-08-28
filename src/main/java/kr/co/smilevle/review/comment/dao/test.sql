2acba7b9f547ff2ea0edb38ad0e7e5f7232bd9b9
JaeKwang Park <qo010478@gmail.com>
2021-08-28 16:45:53 +0900

    tbl_review_comment 테이블 자료형 수정(systimestamp -> varchar2)

--- src/main/java/kr/co/smilevle/review/comment/dao/test.sql
+++ src/main/java/kr/co/smilevle/review/comment/dao/test.sql
@@ -1,12 +1,15 @@
+drop table tbl_review_comment;
+drop sequence review_comment_seq;
+
 create table tbl_review_comment(
 	comment_no number constraint comment_no_pk primary key,
 	review_no number constraint comment_review_no_fk references tbl_review,
 	writer_id varchar2(50) not null,   
-	regdate timestamp not null,
+	regdate varchar2(50) not null,
 	content long not null);  
 
 create sequence review_comment_seq;
 
-insert into tbl_review_comment values(1, 1, 'example123', systimestamp, '댓글 테스트입니다.');
-
 select * from tbl_review_comment;
+
+insert into tbl_review_comment values(2, 1, 'example123', to_char(sysdate + 9/24,'yyyy-mm-dd hh24:mi:ss'), '댓글 테스트입니다.2');
