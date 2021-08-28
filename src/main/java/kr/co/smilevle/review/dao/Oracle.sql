a9770f42e9aee3fbc2f16531788d5c2cd7af1b53
JaeKwang Park <qo010478@gmail.com>
2021-08-28 13:25:40 +0900

    리뷰 댓글 쿼리문 추가

--- src/main/java/kr/co/smilevle/review/dao/Oracle.sql
+++ src/main/java/kr/co/smilevle/review/dao/Oracle.sql
@@ -1,5 +1,3 @@
-drop sequence review_seq;
-
 create table tbl_review(
 	review_no number constraint review_no_pk primary key,
 	writer_id varchar2(50) not null,
@@ -16,6 +14,22 @@ create table tbl_review(
 
 create sequence review_seq;
 
+select * from tbl_review;
+
+create table tbl_review_comment(
+	comment_no number constraint comment_no_pk primary key,
+	review_no number constraint comment_review_no_fk references tbl_review,
+	writer_id varchar2(50) not null,   
+	regdate timestamp not null,
+	content long not null);  
+
+create sequence review_comment_seq;
+
+insert into tbl_review_comment values(1, 1, 'example123', systimestamp, '댓글 테스트입니다.');
+
+select * from tbl_review_comment;
+
+// 리뷰 이의의 데이터 쿼리문
 CREATE TABLE STAY (        
     "STAY_ID" NUMBER(12,0),
     "TITLE" VARCHAR2(600 BYTE),
