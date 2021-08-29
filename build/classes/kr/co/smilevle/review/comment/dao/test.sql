171639b8b2434b6541594e8b84fd44aa857c13a8
JaeKwang Park <qo010478@gmail.com>
2021-08-29 16:24:35 +0900

    원 리뷰글 삭제 시 댓글도 함께 삭제되는 옵션 추가(on delete cascade)

--- build/classes/kr/co/smilevle/review/comment/dao/test.sql
+++ build/classes/kr/co/smilevle/review/comment/dao/test.sql
@@ -5,18 +5,23 @@ delete from TBL_REVIEW_COMMENT;
 
 create table tbl_review_comment(
 	comment_no number constraint comment_no_pk primary key,
-	review_no number constraint comment_review_no_fk references tbl_review,
+	review_no number,
 	writer_id varchar2(50) not null,   
-	regdate date not null,
-	content long not null);  
+	regdate timestamp not null,
+	content long);  
 
 create sequence review_comment_seq;
 
-select * from tbl_review_comment;
+alter table tbl_review_comment 
+	add foreign key (review_no)
+	references tbl_review(review_no)
+	on delete cascade;
 
-insert into tbl_review_comment values(3, 1, 'example123', (sysdate + 9/24), '댓글 테스트입니다.3');
-insert into tbl_review_comment values(4, 1, 'example123', (sysdate + 9/24), '댓글 테스트입니다.4');
-insert into tbl_review_comment values(5, 1, 'example123', (sysdate + 9/24), '댓글 테스트입니다.5');
-insert into tbl_review_comment values(6, 1, 'example123', (sysdate + 9/24), '댓글 테스트입니다.6');
-insert into tbl_review_comment values(7, 1, 'example123', (sysdate + 9/24), '댓글 테스트입니다.7');
-insert into tbl_review_comment values(8, 1, 'example123', (sysdate + 9/24), '댓글 테스트입니다.8');
+insert into tbl_review_comment values(3, 1, 'example123', (systimestamp + 9/24), '댓글 테스트입니다.3');
+insert into tbl_review_comment values(4, 1, 'example123', (systimestamp + 9/24), '댓글 테스트입니다.4');
+insert into tbl_review_comment values(5, 1, 'example123', (systimestamp + 9/24), '댓글 테스트입니다.5');
+insert into tbl_review_comment values(6, 2, 'example123', (systimestamp + 9/24), '댓글 테스트입니다.6');
+insert into tbl_review_comment values(7, 2, 'example123', (systimestamp + 9/24), '댓글 테스트입니다.7');
+insert into tbl_review_comment values(8, 2, 'example123', (systimestamp + 9/24), '댓글 테스트입니다.8');
+
+select * from tbl_review_comment;
