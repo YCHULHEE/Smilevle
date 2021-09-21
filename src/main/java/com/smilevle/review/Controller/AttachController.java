c7a1d05a15cc152fa7027849d12f2f088e51ebb5
JaeKwangPark0712 <qo010478@gmail.com>
2021-09-21 23:15:12 +0900

    리뷰 작성 구현(미완3)
    사진 업로드 가능,
    서버 폴더에 업데이트 할 수 없어서 사진 불러오기 불가능

--- src/main/java/com/smilevle/review/Controller/AttachController.java
+++ src/main/java/com/smilevle/review/Controller/AttachController.java
@@ -1,9 +1,13 @@
 package com.smilevle.review.Controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Locale;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.UUID;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,68 +15,65 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
-import com.oreilly.servlet.multipart.FileRenamePolicy;
+import com.google.gson.JsonObject;
 
 @Controller
 @RequestMapping("/upload")
 public class AttachController {
-
+	
 	@RequestMapping(value = {"", "/"}, method = RequestMethod.POST)
 	@ResponseBody
-	public String uploadAttach(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+	public void uploadAttach(HttpServletRequest request, HttpServletResponse response, @RequestParam MultipartFile upload) throws Exception {
 		final String SAVE_URL = "files";
 		
-		ServletContext context = request.getServletContext();
-		String saveDirectory = context.getRealPath(SAVE_URL);
-		System.out.println("업로드 경로: " + saveDirectory);
+		String saveDirectory = "C:\\Users\\qo010\\git\\Smilevle\\src\\main\\resources\\static\\files";
 		
-		Enumeration names = null;
-		String name = null;
-		String originalFileName = null;
-		String fileSystemName = null;
-		String fileType = null;
-		String fileUrl = null;
+		UUID uid = UUID.randomUUID();
 		
-		int maxPostSize = 10 * 1024 * 1024;
-		String encoding = "UTF-8";
-		FileRenamePolicy policy = new DefaultFileRenamePolicy();
+		OutputStream out = null;
+		PrintWriter printWriter = null;
 		
-		MultipartRequest multi = null;
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/thml;charset=UTF-8");
 		
 		try {
-			multi = new MultipartRequest(request, saveDirectory, maxPostSize, encoding, policy);
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-		names = multi.getFileNames();
-		while(names.hasMoreElements()) {
-			name = (String) names.nextElement();
-			System.out.println("input name: " + name);
+			String fileName = upload.getOriginalFilename();
+			byte[] bytes = upload.getBytes();
 			
-			originalFileName = multi.getOriginalFileName(name);
-			fileSystemName = multi.getFilesystemName(name);
-			fileType = multi.getContentType(name);
+			String ckUploadPath = saveDirectory + File.separator + uid + fileName;
+			System.out.println("ckUploadPath: " + ckUploadPath);
 			
-			fileUrl = request.getContextPath() + "/" + SAVE_URL + "/" + fileSystemName;
-			System.out.println("fileURL: " + fileUrl);
-		}
-		
-		String jsonString = "{\"filename\" : \"" + fileSystemName + "\", \"uploaded\" : 1, \"url\":\"" + fileUrl + "\"}";
-		
-		try {
+			out = new FileOutputStream(new File(ckUploadPath));
+			out.write(bytes);
+			out.flush();
+			
+			printWriter = response.getWriter();
+			String fileUrl = saveDirectory + File.separator + uid + fileName;
+			JsonObject json = new JsonObject();
+			json.addProperty("uploaded", 1);
+			json.addProperty("fileName", fileName);
+			json.addProperty("url", fileUrl);
+			printWriter.println(json);
+			System.out.println("fileUrl: " +fileUrl);
+	
 			request.getSession(false).setAttribute("fileUrl", fileUrl);
-			response.setContentType("application/json; charset=UTF-8");
-			response.getWriter().write(jsonString);
+			
+			printWriter.flush();
 		} catch(IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if(out != null) out.close();
+				if(printWriter != null) printWriter.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			
 		}
-		
-		return null;
+		return;
 	}
 }
